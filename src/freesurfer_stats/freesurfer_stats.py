4796d36a3dbaec29995c7d66195124791ea26cd6
GalBenZvi <hershkovitz1@mail.tau.ac.il>
2022-04-19 18:38:00 +0300

    change stats file validation to a standalone function

--- src/freesurfer_stats/freesurfer_stats.py
+++ src/freesurfer_stats/freesurfer_stats.py
@@ -7,6 +7,7 @@ from typing import Union
 import pandas as pd
 
 from freesurfer_stats.utils import UNITS_CONVERTER
+from freesurfer_stats.utils import validate_stats_file
 
 
 class FreesurferStats:
@@ -26,33 +27,7 @@ class FreesurferStats:
     INDEX_COLUMN = "FieldName"
 
     def __init__(self, stats_file: Union[Path, str]) -> None:
-        self.path = self.validate_stats_file(stats_file)
-
-    def validate_stats_file(self, stats_file: Union[Path, str]) -> Path:
-        """
-        Validate the stats file as an existing file Freesurfer .stats file.
-
-        Parameters
-        ----------
-        stats_file : Union[Path,str]
-            The path to the Freesurfer .stats file.
-
-        Raises
-        ------
-        FileNotFoundError
-            If the stats file does not exist.
-
-        Returns
-        -------
-        Path
-            The path to the stats file.
-        """
-        stats_file = Path(stats_file)
-        if not stats_file.exists():
-            raise FileNotFoundError(f"{stats_file} does not exist")
-        if stats_file.suffix != ".stats":
-            raise ValueError(f"{stats_file} is not a Freesurfer .stats file")
-        return stats_file
+        self.path = validate_stats_file(stats_file)
 
     def get_structural_measurements(self) -> pd.DataFrame:
         """
